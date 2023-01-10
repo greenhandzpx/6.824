73d4ab0f1405809c8efa6573c01f795ae33448c0
greenhandzpx <893522573@qq.com>
2023-01-10 22:36:09 +0800

    lab4b: pass except unreliable tests

--- src/shardkv/server_handler.go
+++ src/shardkv/server_handler.go
@@ -27,26 +27,26 @@ func (kv *ShardKV) handleGetReq(op Op, commandIndex int) {
 		reply.Err = ErrWrongGroup
 		kv.lastApplied = commandIndex
 		//kv.lastApplied++
-		DPrintf("%v(%v) error wrong group, idx:%v", kv.me, kv.gid, commandIndex)
+		DPrintf("%v(%v) get req error wrong group, idx:%v because of migrating, pending config num %v", kv.me, kv.gid, commandIndex, kv.pendingConfig.Num)
 		kv.sendReply(commandIndex, &reply)
 		return
 	}
 
-	//if _, ok := kv.shards[key2shard(op.Key)]; !ok {
-	//	// The config changed, and this shard isn't controlled by this server
-	//	reply.Err = ErrWrongGroup
-	//	return
-	//}
-	//if _, exists := kv.ids[op.Uuid]; !exists {
-	//	kv.ids[op.Uuid] = void{}
-	//}
+	if kv.config.Shards[key2shard(op.Key)] != kv.gid {
+		// this shard isn't controlled by this server
+		reply.Err = ErrWrongGroup
+		DPrintf("%v(%v) get req error wrong group, idx:%v", kv.me, kv.gid, commandIndex)
+		kv.sendReply(commandIndex, &reply)
+		return
+	}
+
 	value, ok := kv.kvs[op.Key]
 	if ok {
 		reply.Err = OK
 		reply.Value = value
-		DPrintf("(idx:%v)%v(%v) get key:%v value:%v", commandIndex, kv.me, kv.gid, op.Key, value)
+		DPrintf("(idx:%v)%v(%v) get key:%v value:%v, config num %v", commandIndex, kv.me, kv.gid, op.Key, value, kv.config.Num)
 	} else {
-		DPrintf("No value for key %v", op.Key)
+		DPrintf("(idx:%v)%v(%v) no value for key %v", commandIndex, kv.me, kv.gid, op.Key)
 		reply.Err = ErrNoKey
 		reply.Value = ""
 	}
@@ -75,15 +75,18 @@ func (kv *ShardKV) handleAppendReq(op Op, commandIndex int) {
 		reply.Err = ErrWrongGroup
 		kv.lastApplied = commandIndex
 		//kv.lastApplied++
-		DPrintf("%v(%v) error wrong group, idx:%v", kv.me, kv.gid, commandIndex)
+		DPrintf("%v(%v) append req error wrong group, idx:%v because of migrating, pending config num %v", kv.me, kv.gid, commandIndex, kv.pendingConfig.Num)
 		kv.sendReply(commandIndex, &reply)
 		return
 	}
-	//if _, ok := kv.shards[key2shard(op.Key)]; !ok {
-	//	// The config changed, and this shard isn't controlled by this server
-	//	reply.Err = ErrWrongGroup
-	//	return
-	//}
+
+	if kv.config.Shards[key2shard(op.Key)] != kv.gid {
+		// this shard isn't controlled by this server
+		reply.Err = ErrWrongGroup
+		DPrintf("%v(%v) append req error wrong group, idx:%v", kv.me, kv.gid, commandIndex)
+		kv.sendReply(commandIndex, &reply)
+		return
+	}
 
 	if _, exists := kv.ids[op.Uuid]; exists && kv.ids[op.Uuid] >= op.Count {
 		DPrintf("count %v already exists", op.Count)
@@ -94,7 +97,7 @@ func (kv *ShardKV) handleAppendReq(op Op, commandIndex int) {
 		if ok {
 			reply.Err = OK
 			kv.kvs[op.Key] += op.Value
-			DPrintf("(idx:%v)%v(%v) append key:%v value:%v", commandIndex, kv.me, kv.gid, op.Key, op.Value)
+			DPrintf("(idx:%v)%v(%v) append key:%v value:%v config num %v", commandIndex, kv.me, kv.gid, op.Key, op.Value, kv.config.Num)
 			DPrintf("%v(%v) now k:%v, v:%v", kv.me, kv.gid, op.Key, kv.kvs[op.Key])
 		} else {
 			reply.Err = ErrNoKey
@@ -127,7 +130,15 @@ func (kv *ShardKV) handlePutReq(op Op, commandIndex int) {
 		reply.Err = ErrWrongGroup
 		kv.lastApplied = commandIndex
 		//kv.lastApplied++
-		DPrintf("%v(%v) error wrong group, idx:%v", kv.me, kv.gid, commandIndex)
+		DPrintf("%v(%v) put req error wrong group, idx:%v because of migrating, pending config num %v", kv.me, kv.gid, commandIndex, kv.pendingConfig.Num)
+		kv.sendReply(commandIndex, &reply)
+		return
+	}
+
+	if kv.config.Shards[key2shard(op.Key)] != kv.gid {
+		// this shard isn't controlled by this server
+		reply.Err = ErrWrongGroup
+		DPrintf("%v(%v) put req error wrong group, idx:%v", kv.me, kv.gid, commandIndex)
 		kv.sendReply(commandIndex, &reply)
 		return
 	}
@@ -141,7 +152,7 @@ func (kv *ShardKV) handlePutReq(op Op, commandIndex int) {
 		DPrintf("count %v already exists", op.Count)
 	} else {
 		kv.ids[op.Uuid] = op.Count
-		DPrintf("(idx:%v)%v(%v) put key:%v value:%v", commandIndex, kv.me, kv.gid, op.Key, op.Value)
+		DPrintf("(idx:%v)%v(%v) put key:%v value:%v config num %v", commandIndex, kv.me, kv.gid, op.Key, op.Value, kv.config.Num)
 		kv.ids[op.Uuid] = op.Count
 		kv.kvs[op.Key] = op.Value
 	}
@@ -182,31 +193,35 @@ func (kv *ShardKV) handleMigrateReq(op Op, commandIndex int) {
 	if _, isLeader := kv.rf.GetState(); !isLeader {
 		DPrintf("%v(%v) isn't leader, shouldn't handle migrate", kv.me, kv.gid)
 		kv.lastApplied = commandIndex
-		return
+		// return
 	}
 
 	DPrintf("%v(%v) get a migrate request %v", kv.me, kv.gid, commandIndex)
 
 	// handoutKvs := make(map[string]string)
+	kv.pendingConfig = op.Conf
 
 	if kv.config.Num == 0 {
+		DPrintf("%v(%v) first conf", kv.me, kv.gid)
 		// means this server just started
-		kv.lastApplied = commandIndex
-		newOp := Op{
-			Type: HANDOUTSHARDS,
-			Conf: op.Conf,
-			// Kvs:  handoutKvs,
-			Kvs:  make(map[string]string),
+		// then just finish the migration
+		for shard, gid := range op.Conf.Shards {
+			if gid == kv.gid {
+				kv.shardVersions[shard] = op.Conf.Num
+			}
 		}
-		kv.rf.Start(newOp)
+		kv.migrating = false
+		kv.config = op.Conf
+		kv.configOutstanding = 0
+		kv.pendingConfig.Num = 0
 		return
 	}
 
 
-	if _, isLeader := kv.rf.GetState(); !isLeader {
-		// only leader fetch shards from other group
-		return
-	}
+	// if _, isLeader := kv.rf.GetState(); !isLeader {
+	// 	// only leader fetch shards from other group
+	// 	return
+	// }
 
 	// update the shards set
 	shouldGetNewShard := false
@@ -215,14 +230,14 @@ func (kv *ShardKV) handleMigrateReq(op Op, commandIndex int) {
 			continue
 		}
 		if kv.config.Shards[shard] == gid {
-			kv.shardVersions[shard] = kv.config.Num
+			kv.shardVersions[shard] = op.Conf.Num
 			continue
 		}
 
 		shouldGetNewShard = true
 
 		if _, isLeader := kv.rf.GetState(); !isLeader {
-			return
+			continue
 		}
 
 		if kv.shardVersions[shard] >= op.Conf.Num {
@@ -231,9 +246,8 @@ func (kv *ShardKV) handleMigrateReq(op Op, commandIndex int) {
 		}
 
 		DPrintf("%v(%v) should have a new shard %v, gid %v",
-			kv.me, kv.gid, shard, gid)
+			kv.me, kv.gid, shard, kv.config.Shards[shard])
 
-		kv.pendingConfig = op.Conf
 
 		args := GetShardsArgs{
 			Shard: shard,
@@ -241,13 +255,13 @@ func (kv *ShardKV) handleMigrateReq(op Op, commandIndex int) {
 			Gid: kv.gid,
 		}
 		targetGid := kv.config.Shards[shard]
+		DPrintf("%v(%v) fetch shards from group %v",
+			kv.me, kv.gid, targetGid)
 		// only the leader sends rpc request to other groups
 		// to fetch shards
 		kv.mu.Unlock()
 		for {
 			success := false
-			DPrintf("%v(%v) fetch shards from group, size:%v",
-				kv.me, kv.gid, len(kv.config.Groups[targetGid]))
 			for _, srv := range kv.config.Groups[targetGid] {
 				server := kv.make_end(srv)
 				var replyTmp GetShardsReply
@@ -291,6 +305,7 @@ func (kv *ShardKV) handleMigrateReq(op Op, commandIndex int) {
 		kv.config = op.Conf
 		kv.configOutstanding = 0
 		kv.pendingConfig.Num = 0
+		DPrintf("%v(%v) finish migrate, conf num %v", kv.me, kv.gid, kv.config.Num)
 	}
 	// // When we fetch all required shards, we send them to the raft
 	// // and let all the other servers get those shards.
@@ -316,41 +331,67 @@ func (kv *ShardKV) handleGetShards(op Op, commandIndex int) {
 		return
 	}
 
-	handoutOp := HandoutShardsArgs {
+	handoutArgs := HandoutShardsArgs {
 		Err: OK,
 		Shard: op.Shard,
 		Gid: kv.gid,
+		Config: op.Conf,
 	}
 
-	if op.Conf.Num < kv.config.Num {
-		// tell the request server to update its config
-		handoutOp.Config = kv.config
 
-	} else if op.Conf.Num > kv.config.Num + 1 {
+	if op.Conf.Num > kv.config.Num + 1 {
 		// our config is too far behind
-		handoutOp.Err = ErrRetry
+		handoutArgs.Err = ErrRetry
 
 	} else {
+		// op.Conf.Num <= kv.config.Num + 1
 		if !kv.migrating &&
 			op.Conf.Num == kv.config.Num + 1 {
 			// update our own config
+			DPrintf("%v(%v) should update config(now %v) when got get_shard(shard %v)request", kv.me, kv.gid,  kv.config.Num, op.Shard)
 			kv.lastMigrateTime = time.Now().UnixMilli()
 			migrateOp := Op {
 				Type: MIGRATE,
 				Conf: op.Conf,
 			}
 			kv.rf.Start(migrateOp)
-			handoutOp.Err = ErrRetry
+			// tell the other group to retry
+			handoutArgs.Err = ErrRetry
 		} else {
+			// handout our shard
+			cnt := 0
+			handoutArgs.Kvs = make(map[string]string)
+			for k, v := range kv.kvs {
+				if key2shard(k) == op.Shard {
+					handoutArgs.Kvs[k] = v
+					cnt++
+				}
+			}
+			DPrintf("%v(%v) send handout shard %v to group %v, kv cnt %v", kv.me, kv.gid, op.Shard, 
+					op.Conf.Shards[op.Shard], cnt)	
 		}
 	}
 
 	// Then send back the handout op and 
 	// put into that server's raft log
 	kv.mu.Unlock()
+	DPrintf("%v(%v) send back handout shard %v to group %v", kv.me, kv.gid, op.Shard, 
+			op.Conf.Shards[op.Shard])	
 	for {
 		success := false
-		// for _, srv := range kv
+		for _, srv := range op.Conf.Groups[op.Conf.Shards[op.Shard]] {
+			server := kv.make_end(srv)
+			var replyTmp HandoutShardsReply
+
+			ok := server.Call("ShardKV.HandoutShards", &handoutArgs, &replyTmp) 
+			if ok && replyTmp.Err == OK {
+				success = true
+				break
+			}
+		}
+		if success {
+			break
+		}
 	}
 
 	kv.mu.Lock()
@@ -369,26 +410,56 @@ func (kv *ShardKV) handleHandoutShards(op Op, commandIndex int) {
 
 	// if op.Conf.Num <= kv.config.Num || op.Conf.Num < kv.configOutstanding {
 	if op.Conf.Num <= kv.config.Num {
-		// outdated shards
+		// outdated conf
+		DPrintf("outdated handout shard %v config num %v in %v(%v)", op.Shard, op.Conf.Num, kv.me, kv.gid)
 		return
 	}
 
 	if op.Conf.Num != kv.pendingConfig.Num {
-		// outdated shards
+		// outdated conf
+		DPrintf("outdated handout shard %v config num %v in %v(%v)", op.Shard, op.Conf.Num, kv.me, kv.gid)
 		return
 	}
 
 	if op.Conf.Num <= kv.shardVersions[op.Shard] {
 		// outdated shard
+		DPrintf("outdated handout shard %v config num %v in %v(%v)", op.Shard, op.Conf.Num, kv.me, kv.gid)
 		return
 	}
 
+	DPrintf("%v(%v) handle handout shards, shard %v conf num %v", kv.me, kv.gid, op.Shard, op.Conf.Num)
+
 	if op.HandoutErr == ErrRetry {	
+		// ErrRetry means the src group is updating its config and we need to 
+		// request again
+		DPrintf("%v(%v) get handout shard %v but should retry", kv.me, kv.gid, op.Shard)
+		if _, isLeader := kv.rf.GetState(); !isLeader {
+			return
+		}
 		args := GetShardsArgs {
 			Shard: op.Shard,
 			Config: kv.pendingConfig,
 			Gid: kv.gid,
 		}
+		kv.mu.Unlock()
+		for {
+			success := false
+			for _, srv := range kv.config.Groups[kv.config.Shards[op.Shard]] {
+				server := kv.make_end(srv)
+				var replyTmp GetShardsReply
+
+				ok := server.Call("ShardKV.GetShards", &args, &replyTmp)
+
+				if ok && replyTmp.Err == OK {
+					success = true
+					break
+				}
+			}
+			if success {
+				break
+			}
+		}
+		kv.mu.Lock()
 		return
 	}
 
@@ -406,7 +477,8 @@ func (kv *ShardKV) handleHandoutShards(op Op, commandIndex int) {
 		}
 		if kv.shardVersions[shard] < kv.pendingConfig.Num {
 			migrateFinished = false
-			break
+			DPrintf("%v(%v) didn't finish migrate yet, conf num %v, miss shard %v", kv.me, kv.gid, kv.config.Num, shard)
+			// break
 		}
 	}
 	if migrateFinished {
@@ -414,6 +486,7 @@ func (kv *ShardKV) handleHandoutShards(op Op, commandIndex int) {
 		kv.migrating = false
 		kv.configOutstanding = 0
 		kv.pendingConfig.Num = 0
+		DPrintf("%v(%v) finish migrate, conf num %v", kv.me, kv.gid, kv.config.Num)
 	}
 	// kv.config = op.Conf
 	// kv.migrating = false
